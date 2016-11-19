b8dd1bed126ffc7f248b6f1e57964a178ec37a3c
Denis M. Gabaidulin <gabaden@gmail.com>
2016-11-19 21:41:42 +0300

    + the first integration test

--- common/src/main/java/org/sherman/task/server/common/domain/TaskType.java
+++ common/src/main/java/org/sherman/task/server/common/domain/TaskType.java
@@ -1,5 +1,15 @@
 package org.sherman.task.server.common.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import org.sherman.task.server.common.util.GuavaCollectors;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.StreamSupport;
+
+import static java.util.function.Function.identity;
+
 /**
  * @author Denis Gabaydulin
  * @since 19.11.16
@@ -7,7 +17,8 @@ package org.sherman.task.server.common.domain;
 public enum TaskType {
     FAILED(1),
     SLEEP(2),
-    PI(3);
+    PI(3),
+    NOT_IMPLEMENTED(4);
 
     private final int id;
 
@@ -18,4 +29,12 @@ public enum TaskType {
     public int getId() {
         return id;
     }
+
+    @JsonCreator
+    public static TaskType fromId(int id) {
+        return indexedById.get(id);
+    }
+
+    private static final Map<Integer, TaskType> indexedById = Arrays.stream(TaskType.values())
+            .collect(GuavaCollectors.toImmutableMap(TaskType::getId, identity()));
 }
